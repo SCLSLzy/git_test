c7c754ce6dc1121aa12aa1107ab99cf14307d531
摆特曼 <@maguohao3707>
2024-06-20 16:05:15 +0800

    11

--- src/main/java/com/atguigu/test/SwhTest.java
+++ src/main/java/com/atguigu/test/SwhTest.java
@@ -8,5 +8,7 @@ package com.atguigu.test;
 public class SwhTest {
     public static void main(String[] args) {
         System.out.println("我是风清扬");
+        System.out.println("i'm batman");
     }
+
 }
