97c6c3e01f06a55a5e9e0e3bae7f8c9f61b20a07
lzy <2438297314@qq.com>
2024-06-20 15:56:22 +0800

    liuzhiyu: update 1

--- src/main/java/com/atguigu/test/Test1.java
+++ src/main/java/com/atguigu/test/Test1.java
@@ -3,5 +3,6 @@ package com.atguigu.test;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("liuzhiyu");
+        System.out.println("liuzhiyu:update1");
     }
 }
