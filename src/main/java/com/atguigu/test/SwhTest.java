22a81c319d47d827cb1eaf1ad22b45ea96b41fde
LAPTOP-FBRT8AIR\Lenovo <3263710399@qq.com>
2024-06-20 15:47:18 +0800

    添加风清扬

---
+++ src/main/java/com/atguigu/test/SwhTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.test;
+
+/**
+ * @作者：swh
+ * @date:2024/6/20 15:43
+ * @description
+ */
+public class SwhTest {
+    public static void main(String[] args) {
+        System.out.println("我是风清扬");
+    }
+}
