0beb07f644917e99e60d523e5eeb823d19f91556
Killuazzz <Killuazzz@777.com>
2024-06-20 16:27:57 +0800

    分支测试

---
+++ src/main/java/com/atguigu/test/RjfTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.test;
+
+/**
+ * ClassName: RjfTest
+ * Package: com.atguigu.test
+ * Description:
+ *
+ * @Author Killuazzz
+ * @Create 2024/6/20 16:27
+ * @Version 1.0
+ */
+public class RjfTest {
+    public static void main(String[] args) {
+        System.out.println("Test:dev_1");
+    }
+}
