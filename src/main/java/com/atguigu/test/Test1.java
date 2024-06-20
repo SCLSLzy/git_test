ad7dc64c458af58d73ac1c852b5fc6dcc70813a0
Killuazzz <Killuazzz@777.com>
2024-06-20 16:17:15 +0800

    添加Killuazzz333

--- src/main/java/com/atguigu/test/Test1.java
+++ src/main/java/com/atguigu/test/Test1.java
@@ -4,6 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("liuzhiyu");
 
-        System.out.println("renjingfeng");
+        System.out.println("renjingfeng333");
     }
 }
