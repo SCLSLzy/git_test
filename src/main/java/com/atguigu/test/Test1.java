3a4bed4de085daf7a06e5f78b2d79c543bb7508a
Killuazzz <Killuazzz@777.com>
2024-06-20 16:15:15 +0800

    添加Killuazzz

--- src/main/java/com/atguigu/test/Test1.java
+++ src/main/java/com/atguigu/test/Test1.java
@@ -3,5 +3,7 @@ package com.atguigu.test;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("liuzhiyu");
+
+        System.out.println("renjingfeng");
     }
 }
